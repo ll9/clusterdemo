03502e6194771cd96352d9519489e9748cb04908
ll9 <llwierer@web.de>
2018-10-16 21:37:32 +0200

    Basic cluster

--- script/layer.js
+++ script/layer.js
@@ -1,10 +1,12 @@
 const vectorSource = new ol.source.Vector();
+const clusterSource = new ol.source.Cluster({source: vectorSource});
 const vectorLayer = new ol.layer.Vector({
-    source: vectorSource
+    source: clusterSource
 });
 
-let geometry = new ol.geom.Point(ol.proj.fromLonLat([12.3739, 47.8136]))
-let feature = new ol.Feature({
-    geometry: geometry
-})
-vectorSource.addFeature(feature);
+for (let i = 0; i < 100; i++) {
+    let feature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([12.3739 + Math.random() / 100, 47.8136 + Math.random() / 100]))
+    })
+    vectorSource.addFeature(feature);
+}
