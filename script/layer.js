137c5c94e858815a761dc721de1c4d38e825e458
ll9 <llwierer@web.de>
2018-10-16 21:50:30 +0200

    applying style to cluster

--- script/layer.js
+++ script/layer.js
@@ -1,7 +1,10 @@
 const vectorSource = new ol.source.Vector();
-const clusterSource = new ol.source.Cluster({source: vectorSource});
+const clusterSource = new ol.source.Cluster({
+    source: vectorSource
+});
 const vectorLayer = new ol.layer.Vector({
-    source: clusterSource
+    source: clusterSource,
+    style: stylefunction
 });
 
 for (let i = 0; i < 100; i++) {
@@ -9,4 +12,27 @@ for (let i = 0; i < 100; i++) {
         geometry: new ol.geom.Point(ol.proj.fromLonLat([12.3739 + Math.random() / 100, 47.8136 + Math.random() / 100]))
     })
     vectorSource.addFeature(feature);
+}
+
+function stylefunction(feature) {
+    if (feature.get('features') != null) {
+        console.log(feature);
+        return new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 10,
+                stroke: new ol.style.Stroke({
+                    color: '#fff'
+                }),
+                fill: new ol.style.Fill({
+                    color: '#3399CC'
+                })
+            }),
+            text: new ol.style.Text({
+                text: feature.get('features').length.toString(),
+                fill: new ol.style.Fill({
+                    color: '#fff'
+                })
+            })
+        });
+    }
 }
